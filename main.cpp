86beea2c6ce7093057cf2998780348c86d2b3138
Ahmed Samir <abozamir@gmail.com>
2016-04-20 19:14:34 +0200

    test 3bhady:

--- main.cpp
+++ main.cpp
@@ -35,6 +35,10 @@ int main()
 		//Update the drawing window
 		AppManager.UpdateInterface();
 
+		//commentat kteer el fatra el gaya 
+		// 
+		// ana //mesh
+		//3arfny
 
 	}while(ActType != EXIT);
 
