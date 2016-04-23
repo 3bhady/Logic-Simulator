8700038287e01228ba10978fc592567c02d79ce7
3bhady <mohamed_abdelhady96@hotmail.com>
2016-04-23 16:11:59 +0200

    2darray of components

--- Components/Component.cpp
+++ Components/Component.cpp
@@ -5,6 +5,14 @@ Component::Component(const GraphicsInfo &r_GfxInfo)
 	m_GfxInfo = r_GfxInfo;	
 }
 
+void Component::AddComponent( Component * Arr[][1400] )
+{
+
+	for ( int i = m_GfxInfo.x1; i < m_GfxInfo.x2; i++ )
+		for ( int j = m_GfxInfo.y1; j < m_GfxInfo.y2; j++ )
+			Arr[i][j] = this;
+}
+
 Component::Component()
 {}
 
