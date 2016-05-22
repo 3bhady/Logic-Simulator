511999840e12975d69105071996d30ed414b226f
3bhady <mohamed_abdelhady96@hotmail.com>
2016-05-22 20:04:21 +0200

    -

---
+++ Actions/Move.cpp
@@ -0,0 +1,31 @@
+#include "Move.h"
+
+
+
+
+
+Move::Move( ApplicationManager * pApp ):Action(pApp )
+{
+
+}
+
+Move::~Move( )
+{
+}
+
+bool Move::ReadActionParameters( )
+{
+	return false;
+}
+
+void Move::Execute( )
+{
+}
+
+void Move::undo( )
+{
+}
+
+void Move::redo( )
+{
+}
